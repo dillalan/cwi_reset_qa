5eaacfe7063cce834fb92aa8f802d7b2a4d0b20c
unknown <alandill@rocketmail.com>
2022-05-21 20:58:45 -0300

    Reestrutura pastas. Cria a pasta runners e suites. Cria classe AllTest e respectiva classe de interface. Testes agora podem ser organizados em tipos/fluxos

---
+++ cwi_qa_reset_challenge_03/src/test/java/br/com/cwi_qa_reset_challenge_03/runners/AllTests.java
@@ -0,0 +1,15 @@
+package br.com.cwi_qa_reset_challenge_03.runners;
+
+import br.com.cwi_qa_reset_challenge_03.TestAPI;
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Categories.class)
+@Categories.IncludeCategory(br.com.cwi_qa_reset_challenge_03.suites.AllTests.class)
+@Suite.SuiteClasses({
+        TestAPI.class
+})
+public class AllTests {
+
+}
